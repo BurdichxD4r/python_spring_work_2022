4e033aa44c454ae7ef5bd938e64f6a68e8fafd7f
BurdichxD4r <burdich.xd4r@gmail.com>
2022-04-13 16:50:47 +0300

    Ещё раз исправил)

--- unit_one/13.04.2022/task11.py
+++ unit_one/13.04.2022/task11.py
@@ -2,8 +2,11 @@
 # Вывести соответствующий ему номер столетия, учитывая, что, к примеру, началом 20 столетия был 1901 год.
 
 year = input('Введите год: ')
-if int(year[-2:]) != 0:
-    print(int(year[:-2])+ 1)
+if len(year) < 3 and int(year) > 0:
+    print('1')
 else:
-    print(int(year[:-2]))
+    if int(year[-2:]) != 0:
+        print(int(year[:-2])+ 1)
+    else:
+        print(int(year[:-2]))
 
