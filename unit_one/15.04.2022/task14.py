ffc1d5f4f27306475be67a723cd32bb0bf45f7ed
BurdichxD4r <burdich.xd4r@gmail.com>
2022-04-15 14:05:19 +0300

    Доделал task14.py

--- unit_one/15.04.2022/task14.py
+++ unit_one/15.04.2022/task14.py
@@ -10,14 +10,19 @@
 # Для числа 1 индексы двух ближайших чисел: 0 и 7
 # Для числа 2 индексы двух ближайших чисел: 6 и 9
 
-Mass = [1,2,17,54,30,89,2,1,6,2]
-Mass_clone = Mass[:]
+Mass = [int(i) for i in input().split()]
+Mass_index = []
 Mass_end = []
-M = []
-namber = int(input('Введите число: '))
-x = 0
-for i in range (Mass.count(namber)):
-    Mass_end.append(Mass_clone.index(namber) + x)
-    Mass_clone.remove(namber)
-    x += 1
-print(Mass_end)
+user_namber = int(input('Введите число из списка: '))
+if user_namber in Mass:
+    for i in range (Mass.count(user_namber)):
+        Mass_index.append(Mass.index(user_namber))
+        Mass.insert(Mass.index(user_namber), False)
+        Mass.remove(user_namber)
+    for i  in range (len(Mass_index) - 1):
+        Mass_end.append(Mass_index[i + 1] - Mass_index[i])
+    print('Для числа', user_namber, 'индексы двух ближайших чисел:',
+          Mass_index[Mass_end.index(min(Mass_end))], 'и',
+          Mass_index[Mass_end.index(min(Mass_end)) + 1])
+else:
+    print('Такого числа нет!')
