419d7eca0282166b6084f27a9f2b082d5507bbe2
BurdichxD4r <burdich.xd4r@gmail.com>
2022-04-25 09:58:49 +0300

    task18.py ready

--- unit_one/20.04.2022/task18.py
+++ unit_one/20.04.2022/task18.py
@@ -8,3 +8,18 @@
 # ........................................
 # Выводы комбинаций оформить в список кортежей.
 
+
+Sum_list = list(int(i) for i in (input('Введите необходимые суммы (через пробел): ').split()))
+combo_list = []
+def combo_side(Sum_input):
+    for side_second in range(1, 7):
+        for side_first in range(1, 7):
+            combo = side_first + side_second
+            if combo == Sum_input:
+                combo_typle = (side_first, side_second)
+                combo_list.append(combo_typle)
+    return combo_list
+for Sum in Sum_list:
+    combo_side(Sum)
+    print('Сумма', Sum, '   комбинация', combo_list)
+    combo_list.clear()
