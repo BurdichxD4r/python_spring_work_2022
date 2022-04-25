c867a59db4507b0ec2fccf0f5ed02c9f0f9916fe
BurdichxD4r <burdich.xd4r@gmail.com>
2022-04-25 19:39:43 +0300

    lesson_9 classwork

--- unit_one/25.04.2022/task23.py
+++ unit_one/25.04.2022/task23.py
@@ -3,4 +3,13 @@
 # Попробуйте все возможные сдвиги и расшифруйте фразу.
 
 
-# grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin.
+string_code = "grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin."
+string = "grznuamnzngzcgesgetuzhkuhbouaygzloxyzatrkyyeuaxkjazin"
+alphabet = []
+while string != '':
+    alphabet.append(string[0])
+    string = string.replace(string[0], '')
+print(alphabet)
+for letter in alphabet:
+    string_code_ready = string_code.replace(letter, alphabet[alphabet.index(letter) - 1])
+print(string_code_ready)
