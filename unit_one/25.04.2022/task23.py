b22d58f65845c956044121acec44f667806cc887
BurdichxD4r <burdich.xd4r@gmail.com>
2022-05-11 17:31:44 +0300

    task23.py ready

--- unit_one/25.04.2022/task23.py
+++ unit_one/25.04.2022/task23.py
@@ -4,26 +4,17 @@
 
 
 string_code = "grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin."
-string = "grznuamnzngzcgesgetuzhkuhbouaygzloxyzatrkyyeuaxkjazin"
-alphabet = []
-while string != '':
-    alphabet.append(string[0])
-    string = string.replace(string[0], '')
-alphabet = alphabet + alphabet
-string = "grznuamnzngzcgesgetuzhkuhbouaygzloxyzatrkyyeuaxkjazin"
-string_list = list(string[:])
-letter_index = 0
-for letter in string_list:
-    if letter in alphabet:
-        string_list[letter_index] = alphabet[alphabet.index(letter) + 3]
-        letter_index += 1
-print(string_list)
-string_ready = ''
-kostil = 0
-for index in range(len(string_code)):
-    if string_code[index] != ' ' and "'" and  ',':
-        string_ready = string_ready + string_list[index + kostil]
-    else:
-        string_ready = string_ready + string_code[index]
-        kostil -= 1
-print(string_ready)
+string = ''
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+def cezar(user_string):
+    string = ''
+    for i in range(len(user_string)):
+        string += '*'
+    for i in range(len(user_string)):
+        if user_string[i] in alphabet:
+            string = string.replace('*', alphabet[alphabet.index(user_string[i]) - 6], 1)
+        else:
+            string = string.replace('*', user_string[i], 1)
+    return string
+print(cezar(string_code))
