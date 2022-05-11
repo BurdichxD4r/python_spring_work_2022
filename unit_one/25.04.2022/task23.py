dec2d85d071ccc46bd5d8a8b66f092dfb1147d3c
BurdichxD4r <burdich.xd4r@gmail.com>
2022-05-11 17:34:09 +0300

    task23.py ready

--- unit_one/25.04.2022/task23.py
+++ unit_one/25.04.2022/task23.py
@@ -4,7 +4,6 @@
 
 
 string_code = "grznuamn zngz cge sge tuz hk uhbouay gz loxyz atrkyy eua'xk jazin."
-string = ''
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 def cezar(user_string):
