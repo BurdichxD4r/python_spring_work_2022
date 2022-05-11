6765ba0c25df896492e3d724993724c10f8a1ffd
BurdichxD4r <burdich.xd4r@gmail.com>
2022-05-11 19:29:39 +0300

    task25.py ready

--- unit_one/25.04.2022/task25.py
+++ unit_one/25.04.2022/task25.py
@@ -5,3 +5,19 @@
 # Input             	            Output
 # apple	                            aple
 # 25.04.2022 Good morning !!	    godmrni
+
+
+string_1 = 'apple'
+string_2 = '25.04.2022 Good morning !!'
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+def code(text):
+    text = text.lower()
+    new_text = ''
+    for i in range(len(text)):
+        if text[i] in alphabet:
+            if text[i] not in new_text:
+                new_text += text[i]
+    return new_text
+print(code(string_1))
+print(code(string_2))
